9f89307fa30724fe25396c128604a9b1e8cb9d36
baonv <vietbao4htk@gmail.com>
2016-03-26 06:14:23 +0000

    Create new Pics action and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,20 @@
 class PicsController < ApplicationController
+    
 def index
+end
+
+def new
+@pic = Pic.new
+end
+
+def crete
+    @pic= Pic.new(pic_params)
+end
+
+private
+
+def pic_params
+param.require(:pic).permit(:title,:description)
 end
 
 end
